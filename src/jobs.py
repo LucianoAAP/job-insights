4a1fe593f7e7307707589838cb24023ab0518733
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-02-11 14:48:07 -0300

    adiciona requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,7 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-
-    return []
+    jobs_list = []
+    with open(path) as file:
+        jobs = csv.DictReader(file)
+        for job in jobs:
+            jobs_list.append(job)
+    return jobs_list
